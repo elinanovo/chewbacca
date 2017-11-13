0cd0551bf50fda6d6ad39bb8af652b4e871f3452
elinanovo <31651250+elinanovo@users.noreply.github.com>
2017-11-13 18:13:49 +0400

    Add files via upload

---
+++ hw3/hw3.py
@@ -0,0 +1,16 @@
+#вариант №7
+letters=input()
+spisok=list(letters)
+print (letters)
+for i in range (0, len (letters)):
+    print(letters [i:-i])
+#длинный скучный вариант №2
+#vtoraya=letters [1:-1]
+#print (vtoraya)
+#tretya=letters [2:-2]
+#print (tretya)
+#chetvertaya=letters [3:-3]
+#print (chetvertaya)
+#pyataya=letters [4:-4]
+#print (pyataya)
+#print (letters[5])
